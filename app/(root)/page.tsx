28d513d87f92283f1d9f6faadf8991fe9b6e07bb
Jyotirmoy <jyotirmoykarmakar02@gmail.com>
2025-02-17 20:26:48 -0700

    Home Data

---
+++ app/(root)/page.tsx
@@ -0,0 +1,17 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { Button } from "@/components/ui/button";
+import { sampleBooks } from "@/constants";
+import Image from "next/image";
+
+const Home = () => {
+  return (
+    <>
+      <BookOverview {...sampleBooks[0]} />
+
+      <BookList title='title' books={sampleBooks} containerClassName='mt-28' />
+    </>
+  );
+};
+
+export default Home;
